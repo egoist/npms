25b71aecc49dfbd435881815937a62b8036a897e
vai <ncysatnaf@users.noreply.github.com>
2016-07-13 10:22:24 -0500

    add searchHistory (#9)
    * add searchHistory
    
    * bug fix
    
    * fix lint issues

--- app/containers/main.js
+++ app/containers/main.js
@@ -1,42 +1,33 @@
 import React, {Component} from 'react'
 import {
-  TextInput,
   StatusBar,
-  View
+  View,
+  Text,
+  TouchableOpacity
 } from 'react-native'
 
 import vars from '../styles/vars'
 import styles from '../styles/main'
 
 export default class MainPage extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      text: ''
-    }
+  handleSearch() {
+    this.props.navigator.push({
+      name: 'result'
+    })
   }
 
-  async handleSearch() {
-    this.props.navigator.push({
-      name: 'result',
-      text: this.state.text
-    })
-    this.setState({
-      text: ''
-    })
-  }
   render() {
     return (
       <View style={styles.container}>
         <StatusBar
           backgroundColor={vars.mainColor} />
-        <TextInput
-          style={styles.searchInput}
-          placeholder="Type here to search modules..."
-          placeholderTextColor="white"
-          value={this.state.text}
-          onChangeText={text => this.setState({text})}
-          onSubmitEditing={() => this.handleSearch()} />
+        <TouchableOpacity
+          onPress={() => this.handleSearch()}
+          style={styles.searchInput}>
+          <Text style={styles.searchInputText}>
+            "Type here to search modules..."
+          </Text>
+        </TouchableOpacity>
       </View>
     )
   }
